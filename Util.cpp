cc4b22d60d6054061628f011df7e6b3dbf597474
Daniel Dunbar <daniel@zuster.org>
2011-10-22 12:11:35 -0700

    Add get_elapsed_time_in_seconds util (from an arbitrary start point).

--- Util.cpp
+++ Util.cpp
@@ -1,7 +1,13 @@
 #include <sys/time.h>
 
-double get_time_in_seconds(void) {
+double get_time_in_seconds() {
   struct timeval t;
   gettimeofday(&t, NULL);
   return (double) t.tv_sec + t.tv_usec * 1.e-6;
+}
+
+double get_elapsed_time_in_seconds() {
+  static double start_time = get_time_in_seconds();
+
+  return get_time_in_seconds() - start_time;
 }
--- Util.h
+++ Util.h
@@ -3,6 +3,8 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-double get_time_in_seconds(void);
+double get_time_in_seconds();
+
+double get_elapsed_time_in_seconds();
 
 #endif // UTIL_H
